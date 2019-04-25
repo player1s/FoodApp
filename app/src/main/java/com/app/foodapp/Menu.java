30d73a7437816124678f57dde1b71ca66d006c58
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-04-25 16:25:12 +0200

    Button son activities

--- app/src/main/java/com/app/foodapp/Menu.java
+++ app/src/main/java/com/app/foodapp/Menu.java
@@ -3,11 +3,14 @@ package com.app.foodapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
 }
