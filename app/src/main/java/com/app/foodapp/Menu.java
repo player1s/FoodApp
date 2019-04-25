869508d77adb04422b2796a7fcbae36dcd08a1fe
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-04-25 20:43:56 +0200

    added the first db query

--- app/src/main/java/com/app/foodapp/Menu.java
+++ app/src/main/java/com/app/foodapp/Menu.java
@@ -1,12 +1,18 @@
 package com.app.foodapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class Menu extends AppCompatActivity {
 
@@ -15,7 +21,7 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         final Button button = findViewById(R.id.btnMenuSelectFood);
         button.setOnClickListener(new View.OnClickListener() {
@@ -23,6 +29,22 @@ public class Menu extends AppCompatActivity {
                 Intent intent = new Intent(getBaseContext(), InredientsSelector.class);
                 //intent.putExtra(EXTRA_MESSAGE, message);
                 startActivity(intent);
+
+                db.collection("Restaurants")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        System.out.println(document.getId() + " => " + document.getData());
+                                    }
+                                } else {
+                                    System.out.println("Error getting documents." + task.getException());
+                                }
+                            }
+                        });
+
             }
         });
     }
