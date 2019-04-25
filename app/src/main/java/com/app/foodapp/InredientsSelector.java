09962c5152e688c39db5441f0d38c5109162c870
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-04-25 22:02:11 +0200

    progress towards networking

--- app/src/main/java/com/app/foodapp/InredientsSelector.java
+++ app/src/main/java/com/app/foodapp/InredientsSelector.java
@@ -25,6 +25,9 @@ public class InredientsSelector extends AppCompatActivity {
             }
         });
 
+
+        
+
        /* Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
