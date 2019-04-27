6227285a1f021ecdb7dc2c236e3b33faa9dec26e
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-04-27 17:32:13 +0200

    CompleteNavigation

--- app/src/main/java/com/app/foodapp/Logic/OrderConfirmed/OrderConfirmed.java
+++ app/src/main/java/com/app/foodapp/Logic/OrderConfirmed/OrderConfirmed.java
@@ -1,8 +1,12 @@
 package com.app.foodapp.Logic.OrderConfirmed;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.app.foodapp.Logic.MainActivityMenu.Menu;
 import com.app.foodapp.R;
 
 public class OrderConfirmed extends AppCompatActivity {
@@ -11,6 +15,15 @@ public class OrderConfirmed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_confirmed);
+
+        final Button button = findViewById(R.id.btnOrderConfirmedBackToMenu);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), Menu.class);
+                //intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        });
 
     }
 
--- app/src/main/java/com/app/foodapp/Logic/Settings/Settings.java
+++ app/src/main/java/com/app/foodapp/Logic/Settings/Settings.java
@@ -1,8 +1,13 @@
 package com.app.foodapp.Logic.Settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.app.foodapp.Logic.MainActivityMenu.Menu;
+import com.app.foodapp.Logic.OrderConfirmed.OrderConfirmed;
 import com.app.foodapp.R;
 
 public class Settings extends AppCompatActivity {
@@ -11,6 +16,15 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        final Button button = findViewById(R.id.btnSettingsBackToMenu);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), Menu.class);
+                //intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        });
 
     }
 
