8b95fdd7cdda3434587e25b99ca909e61431f00c
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-04-28 16:30:53 +0200

    functional bottom navbar, added breakfast and dinner foods

--- app/src/main/java/com/app/foodapp/Logic/MainActivityMenu/Menu.java
+++ app/src/main/java/com/app/foodapp/Logic/MainActivityMenu/Menu.java
@@ -1,11 +1,14 @@
 package com.app.foodapp.Logic.MainActivityMenu;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 
 import com.app.foodapp.Logic.IngredientsSelector.InredientsSelector;
@@ -21,82 +24,47 @@ import java.util.Map;
 
 public class Menu extends AppCompatActivity implements MenuItemAdapter.OnListItemClickListener{
 
-    RecyclerView mPokemonList;
+    RecyclerView mItemList;
     MenuItemAdapter mMenuItemAdapter;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        mPokemonList = findViewById(R.id.recview);
-        mPokemonList.hasFixedSize();
-        mPokemonList.setLayoutManager(new LinearLayoutManager(this));
+        mItemList = findViewById(R.id.recview);
+        mItemList.hasFixedSize();
+        mItemList.setLayoutManager(new LinearLayoutManager(this));
 
+        fireStoreRead("/Restaurants/Restaurant1/Lunch");
 
+        //---------------Bottom navbar navigation start-----------------------------------------------------
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        final ArrayList<MenuItem> menuItems = new ArrayList<>();
-
-        //-----------------Firestore read start---------------------------------------------------------------
-
-        final FirebaseFirestore db = FirebaseFirestore.getInstance();
-        final MenuItemAdapter.OnListItemClickListener listener = this;
-
-        db.collection("/Restaurants/Restaurant1/Menu").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task)  {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
+            public boolean onNavigationItemSelected(@NonNull android.view.MenuItem item) {
+                System.out.println("breakfast itemid: " + R.id.btnBottomNavigationMenuBreakfast);
+                System.out.println("lunch itemid: " + R.id.btnBottomNavigationMenuLunch);
+                System.out.println("dinner itemid: " + R.id.btnBottomNavigationMenuDinner);
 
-                        MenuItem menuItem = new MenuItem(null, 0, 0, 0);
+                System.out.println("it thinks itemid: " + item.getItemId());
 
-                        Map<String, Object> map = document.getData();
-                        for (Map.Entry<String, Object> entry : map.entrySet()) {
-                            System.out.println(entry.getKey() + "/" + entry.getValue());
-                            if(entry.getKey().equals("Id")){
-
-                                menuItem.setId(Integer.parseInt(entry.getValue().toString()));
-                            }
-                            if(entry.getKey().equals("PicId")){
-
-                                menuItem.setIconId(Integer.parseInt(entry.getValue().toString()));
-                            }
-                            if(entry.getKey().equals("name")){
-
-                                menuItem.setName(entry.getValue().toString());
-                            }
-                            if(entry.getKey().equals("Price")){
-
-                                menuItem.setPrice(Integer.parseInt(entry.getValue().toString()));
-                            }
-                        }
-
-                        menuItems.add(menuItem);
-
-                        System.out.println(document.getId() + " => " + document.getData());
-                    }
-                } else {
-                    System.out.println("Error getting documents." + task.getException());
+                switch (item.getItemId()) {
+                    case 2131230759:
+                        fireStoreRead("/Restaurants/Restaurant1/Breakfast");
+                        break;
+                    case 2131230839:
+                        fireStoreRead("/Restaurants/Restaurant1/Lunch");
+                        break;
+                    case 2131230797:
+                        fireStoreRead("/Restaurants/Restaurant1/Dinner");
+                        break;
                 }
-
-/*
-                int resid = R.drawable.respizza;
-                System.out.println(resid);
-                int resid2 = R.drawable.resgoulash;
-                System.out.println(resid2);
-                int resid3 = R.drawable.reslasagna;
-                System.out.println(resid3);
-                int resid4 = R.drawable.resccc;
-                System.out.println(resid4);
-                // finding out the id of the given pic
-*/
-
-                mMenuItemAdapter = new MenuItemAdapter(menuItems, listener);
-                mPokemonList.setAdapter(mMenuItemAdapter);
+                return true;
             }
         });
-        //---------------Firestore read end-----------------------------------------------------------------
     }
 
     @Override
@@ -110,4 +78,73 @@ public class Menu extends AppCompatActivity implements MenuItemAdapter.OnListIte
         startActivity(intent);
 
     }
+    //---------------Bottom navbar navigation end-----------------------------------------------------
+
+    public void fireStoreRead(String collectionPath)
+    {
+        //-----------------Firestore read start---------------------------------------------------------------
+
+        final ArrayList<MenuItem> menuItems = new ArrayList<>();
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final MenuItemAdapter.OnListItemClickListener listener = this;
+
+        db.collection(collectionPath).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task)  {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+                        MenuItem menuItem = new MenuItem(null, 0, 0, 0);
+                        //------------------define the type of the read data start --------------
+                        Map<String, Object> map = document.getData();
+                        for (Map.Entry<String, Object> entry : map.entrySet()) {
+                            System.out.println(entry.getKey() + "/" + entry.getValue());
+                            if(entry.getKey().equals("id")){
+
+                                menuItem.setId(Integer.parseInt(entry.getValue().toString()));
+                            }
+                            if(entry.getKey().equals("PicId")){
+
+                                menuItem.setIconId(Integer.parseInt(entry.getValue().toString()));
+                            }
+                            if(entry.getKey().equals("name")){
+
+                                menuItem.setName(entry.getValue().toString());
+                            }
+                            if(entry.getKey().equals("price")){
+
+                                menuItem.setPrice(Integer.parseInt(entry.getValue().toString()));
+                            }
+                        }
+                        //------------------define the type of the read data end --------------
+                        menuItems.add(menuItem);
+
+                        System.out.println(document.getId() + " => " + document.getData());
+                    }
+                } else {
+                    System.out.println("Error getting documents." + task.getException());
+                }
+
+
+                int resid = R.drawable.respizza;
+                System.out.println(resid);
+                int resid2 = R.drawable.ressandwich;
+                System.out.println(resid2);
+                /*
+                int resid3 = R.drawable.reslasagna;
+                System.out.println(resid3);
+                int resid4 = R.drawable.resccc;
+                System.out.println(resid4);
+                // finding out the id of the given pic
+*/
+
+                mMenuItemAdapter = new MenuItemAdapter(menuItems, listener);
+                mItemList.setAdapter(mMenuItemAdapter);
+            }
+        });
+        //---------------Firestore read end-----------------------------------------------------------------
+
+    }
+
+
 }
